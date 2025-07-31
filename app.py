c808168bb0ab46f3beef08a2a153d5c5d0191973
Zechariah <zechariahwong@gmail.com>
2025-07-31 08:52:10 +0000

    Adds a few files

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, Zechariah!!!"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    app.run(port=port, host="0.0.0.0")
